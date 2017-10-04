0fd0df32650bf53f4d7715fc0ada7ada4098e879
Tyron Ferreira <tyron.ferreira@epitech.eu>
2017-10-04 20:26:29 +0200

    commit

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,14 @@
-cp .vim /home/ubuntu/.vim
-cp /home/ubuntu/.vim/vimrc .vimrc
 git clone https://github.com/tpope/vim-pathogen
-cp vim-pathogen/autoload/pathogen.vim /home/ubuntu/.vim/autoload/
-
+cp vim-pathogen/autoload/pathogen.vim .vim/autoload/
 rm -rf  .vim/bundle/*
-git clone https://github.com/scrooloose/nerdtree /home/ubuntu/.vim/bundle/
-git clone https://github.com/derekwyatt/vim-scala /home/ubuntu/.vim/bundle/
+
+
+git clone https://github.com/scrooloose/nerdtree .vim/bundle/nerdtree
+git clone https://github.com/derekwyatt/vim-scala .vim/bundle/vim-scala
+
+
+cp .vim /home/ubuntu/
+cp /home/ubuntu/.vim/vimrc /home/ubuntu/vimr333
+
+rm -rf vim-pathogen
 
